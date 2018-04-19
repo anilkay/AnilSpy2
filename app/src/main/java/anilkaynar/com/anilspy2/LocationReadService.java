968034b4b63b320363ce87ad779a1a13dd106fc5
Anıl Kaynar <aanilkay@gmail.com>
2018-04-19 18:39:37 +0300

    Location Change Work Sort of

---
+++ app/src/main/java/anilkaynar/com/anilspy2/LocationReadService.java
@@ -0,0 +1,82 @@
+package anilkaynar.com.anilspy2;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+public class LocationReadService extends Service {
+    LocationListener[] mLocationListeners = new LocationListener[]{
+            new LocationListener(LocationManager.PASSIVE_PROVIDER)
+    };
+
+    @Override
+    public void onCreate() {
+        initLocalizationService();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            Log.e("Houston there was a problem","Houston there has a big problem");
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER,
+                1000, 1000, mLocationListeners[0]);
+    }
+
+    LocationManager  mLocationManager;
+    public LocationReadService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+       return  null;
+    }
+    private class LocationListener implements android.location.LocationListener{
+        Location mLastLocation;
+        public LocationListener(String passiveProvider) {
+            mLastLocation = new Location(passiveProvider);
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Double latitude=location.getLatitude();
+            Double longitude=location.getLongitude();
+            Log.e("On Location change",latitude+" "+longitude);
+            //Locationu bu şekilde alabiliyorsak ne ala
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+
+    }
+    public void initLocalizationService(){
+        mLocationManager = (LocationManager) getApplicationContext().
+                getSystemService(Context.LOCATION_SERVICE);
+
+    }
+}
--- app/src/main/java/anilkaynar/com/anilspy2/MainActivity.java
+++ app/src/main/java/anilkaynar/com/anilspy2/MainActivity.java
@@ -1,7 +1,9 @@
 package anilkaynar.com.anilspy2;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +14,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent=new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
         startActivity(intent);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION
+                ,Manifest.permission.ACCESS_COARSE_LOCATION
+                },
+                //Ne zaman bir location change algılansa çağırılıyor.
+
+                4);
         Intent notificationServiceIntent=new Intent(MainActivity.this,
                       NotificationService.class);
         MainActivity.this.startService(notificationServiceIntent);
+        Intent locationServiceIntent=new Intent(MainActivity
+                .this,LocationReadService.class);
+        MainActivity.this.startService(locationServiceIntent);
     }
 }
